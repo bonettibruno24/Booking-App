b70ca9dc40da5472645e3d8ae5b723bc73f6f391
!--2B--! <brunobonetti.silva@gmail.com>
2024-02-12 12:14:31 -0300

    feate: create and alterate repositorys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
+import { BackgroundComponent } from './components/background/background.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    BackgroundComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
