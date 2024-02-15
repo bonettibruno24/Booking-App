9af553ed5c4afc2494cb110cae2333e710e87e79
!--2B--! <brunobonetti.silva@gmail.com>
2024-02-14 23:47:17 -0300

    feat: buttons in process

--- src/app/components/card-list/card-list.component.ts
+++ src/app/components/card-list/card-list.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-list.component.scss']
 })
 export class CardListComponent {
-
+  styleBorder = {
+    'border': '2px solid #55555599'
+  }
 }
